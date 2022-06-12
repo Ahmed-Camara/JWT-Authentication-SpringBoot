5f249b23d3699cdbe0758f501dbd52d81e3ea103
Ahmed-Camara <camarahmed123@gmail.com>
2022-06-12 18:29:21 +0000

    RoleRepository

--- src/main/java/com/SpringJWTSecurity/model/RoleModel.java
+++ src/main/java/com/SpringJWTSecurity/model/RoleModel.java
@@ -6,12 +6,12 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class RoleModel {
-	private String id;
+	private Long id;
 	private String roleName;
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
-	public void setId(String id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 	public String getRoleName() {
